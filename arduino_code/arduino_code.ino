8d65d2c1a708086d7f467b2d107952470f2a647c
Nico <76497980+Nischu2@users.noreply.github.com>
2025-06-17 22:56:57 +0200

    removed testing code

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -4,23 +4,7 @@ void setup() {
 
   initializeLeds();
   initializeSensors();
-
-  // testing leds, remove later
-  displayCO2(2);
-  displayHumidity(6);
-  // testing leds end
 }
 
 void loop() {
-  // testing sensors, remove later
-  float h = readHumidity();
-  float t = readTemperature();
-  Serial.print("Luftfeuchtigkeit: ");
-  Serial.print(h);
-  Serial.print(" %\t");
-  Serial.print("Temperatur: ");
-  Serial.print(t);
-  Serial.println(" *C");
-  delay(2000);
-  // testing sensors end
 }
