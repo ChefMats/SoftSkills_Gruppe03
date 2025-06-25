e30c5152455117dc70e59411f696d8b02870ce8f
Nico <76497980+Nischu2@users.noreply.github.com>
2025-06-25 11:39:37 +0200

    fixed some variable names

--- arduino_code/local_sensors.ino
+++ arduino_code/local_sensors.ino
@@ -1,29 +1,28 @@
 #include "DHT.h"
 #include <MQUnifiedsensor.h>
 
-#define DHTPIN D4
-#define DHTTYPE DHT11
+#define DHT_PIN D4
+#define DHT_TYPE DHT11
 
 #define MQ135PIN A0
-#define Board "ESP8266"
-#define Voltage_Resolution 3.3
-#define ADC_Bit_Resolution 10
-#define RatioMQ135CleanAir 3.6
+#define BOARD "ESP8266"
+#define VOLTAGE_RESOLUTION 3.3
+#define ADC_BIT_RESOLUTION 10
 #define CO2CalibrationValue 31.0
-#define regressionMethod 1
-#define aValue 110.47
-#define bValue -2.862
+#define REGRESSION_METHOD 1
+#define A_VALUE 110.47
+#define B_VALUE -2.862
 
-DHT dht(DHTPIN, DHTTYPE);
+DHT dht(DHT_PIN, DHT_TYPE);
 
-MQUnifiedsensor MQ135(Board, Voltage_Resolution, ADC_Bit_Resolution, MQ135PIN, "MQ-135");
+MQUnifiedsensor MQ135(BOARD, VOLTAGE_RESOLUTION, ADC_BIT_RESOLUTION, MQ135PIN, "MQ-135");
 
 void initializeSensors(){
   dht.begin();
 
-  MQ135.setRegressionMethod(regressionMethod);
-  MQ135.setA(aValue);
-  MQ135.setB(bValue);
+  MQ135.setRegressionMethod(REGRESSION_METHOD);
+  MQ135.setA(A_VALUE);
+  MQ135.setB(B_VALUE);
   MQ135.init();
   
   MQ135.setR0(CO2CalibrationValue);
