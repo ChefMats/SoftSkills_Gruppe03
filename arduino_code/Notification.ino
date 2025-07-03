82e12fb479b751d174b61ea5b5507a5b2d9a57ed
ChefMats <mertenmats@gmail.com>
2025-07-03 11:29:32 +0200

    Insidedata_widgets

--- arduino_code/Notification.ino
+++ arduino_code/Notification.ino
@@ -1,5 +1,43 @@
 #include "config.h"
-#include <BlynkSimpleEsp8266.h>  
+#include <BlynkSimpleEsp8266.h>
+
+
+float readTemperature();
+float readHumidity();
+float readCO2();
+
+void checkIndoorNotifications() {
+  float tempinside = readTemperature();
+  float humidityinside = readHumidity();
+  float co2ppminside = readCO2();
+
+  
+//für widgets
+  Blynk.virtualWrite(V1, tempinside);     
+  Blynk.virtualWrite(V2, humidityinside); 
+  Blynk.virtualWrite(V3, co2ppminside);
+
+
+bool notificationSent = false;
+
+if (co2ppminside > 1000) {
+  Blynk.logEvent("co2_warnung", "CO₂-Wert ist zu hoch! Bitte lüften.");
+  notificationSent = true;
+}
+
+if (humidityinside < 30) {
+  Blynk.logEvent("trocken", "Luftfeuchtigkeit zu niedrig — evtl. Luftbefeuchter nutzen.");
+  notificationSent = true;
+}
+
+if (humidityinside > 70) {
+  Blynk.logEvent("feucht", "Luftfeuchtigkeit zu hoch — evtl. lüften.");
+  notificationSent = true;
+}
+
+if (!notificationSent) {
+  Serial.println("Keine Benachrichtigung für drinnen notwendig.");
+}
 
 //draußen daten
 void notifyOutsideData(float tempC, int humidity, const char* weatherDesc) {
@@ -8,13 +46,7 @@ void notifyOutsideData(float tempC, int humidity, const char* weatherDesc) {
   } else if (humidity < 60 && tempC < 24) {
     Blynk.logEvent("lueften", "Gute Bedingungen zum Lüften!");
   } else {
-    Serial.println("Keine Benachrichtigung notwendig.");
+    Serial.println("Keine Benachrichtigung für draußen notwendig.");
   }
 }
 
-// Indoor daten beispiel (später)
-void notifyIndoorData(int co2ppm, int humidityInnen) {
-  if (co2ppm > 1000) {
-    Blynk.logEvent("co2_warnung", "CO₂-Wert ist zu hoch! Bitte lüften.");
-  }
-}
