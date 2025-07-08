7fe93547d46e831e37514c4b74016544672568ac
Schultp00 <schultp@outlook.de>
2025-07-08 14:46:45 +0200

    Button + Speaker

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -7,6 +7,7 @@
 bool buttonIsPressed = false;
 bool ledsEnabled = true;  // LED-Ring anfangs an
 double lastMillis = 0;
+double lastAlertMillis = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -20,12 +21,15 @@ void setup() {
 }
 
 void loop() {
+  unsigned long now = millis();
+
   if (millis() - lastMillis >= 2000) {
     lastMillis = millis();
     Blynk.run();
     fetchWeatherData();
     printIndoorData();
     ledsetup();
+    initializeSpeaker();
   }
 
   // Button-Status prüfen & toggeln
@@ -48,7 +52,18 @@ void loop() {
     buttonIsPressed = false;
     delay(200);  // debounce
   }
+  
+  
+  float co2 = readCO2();
+  if (co2 > 850 && (now - lastAlertMillis >= 5000)) {
+    lastAlertMillis = now;
+    Serial.println("CO₂ > 850ppm — Warnton!");
+    playAlert(1000);
+  }
+
 }
+
+
 
 void ledsetup() {
   if (!ledsEnabled) return;  // wenn LEDs aus, nichts tun
