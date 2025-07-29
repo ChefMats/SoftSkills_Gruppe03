989167a0559e0439ac2cc6a539b92c98c974a275
Schultp00 <schultp@outlook.de>
2025-07-29 19:38:19 +0200

    Config änderung + LED Range

--- arduino_code/config.h
+++ arduino_code/config.h
@@ -10,15 +10,15 @@ const char* password = "SmartBuddy";
 // ------------------------
 // Wetterdaten
 // ------------------------
-const char* owm_api_key = "API_KEY";  // <- von OpenWeatherMap
+const char* owm_api_key = "";  // <- von OpenWeatherMap
 const char* city = "Oldenburg";
 const char* country_code = "de";
 
 // ------------------------
 // Blynk-Konfiguration (für Handy-Benachrichtigungen)
 // ------------------------
-#define BLYNK_TEMPLATE_ID "TMPL4E5ra_ND-"
-#define BLYNK_TEMPLATE_NAME "AirBuddy"
+#define BLYNK_TEMPLATE_ID ""
+#define BLYNK_TEMPLATE_NAME ""
 #define BLYNK_AUTH_TOKEN ""
 
 #endif
