970854a87cf58af0481a90a6a4ace6d34b9dad54
Schultp00 <schultp@outlook.de>
2025-07-29 19:50:48 +0200

    Weitere LED Anpassung+ Benachrichtungsnachricht

--- arduino_code/Notification.ino
+++ arduino_code/Notification.ino
@@ -8,7 +8,7 @@ void notifyOutsideData(float tempC, int humidity, const char* weatherDesc) {
   } else if (humidity < 60 && tempC < 24) {
     Blynk.logEvent("outsideluft1", "Gute Bedingungen zum Lüften!");
   } else {
-    Serial.println("Keine spezielle Lüftungsbenachrichtigung notwendig.");
+    Serial.println("Alles in Ordnung. Genieße deinen Tag!");
   }
 }
 
@@ -19,8 +19,8 @@ void notifyIndoorData(float tempInnen, int humidityInnen, int co2ppm) {
   Blynk.virtualWrite(V2, (int)humidityInnen);
   Blynk.virtualWrite(V3, (int)co2ppm);
 
-  // CO₂-Warnung bei 850ppm
-  if (co2ppm > 850) {
+  // CO₂-Warnung bei 500ppm
+  if (co2ppm > 500) {
     Blynk.logEvent("co2_warnung", "CO₂-Wert ist zu hoch! Bitte lüften.");
   }
 }
